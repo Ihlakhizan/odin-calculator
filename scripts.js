f0d807906dc1529989dd4bfc0d451df7f6bc9254
Manoranjan Kumar <manoranjan.kumar@outlook.com>
2022-12-03 15:59:55 +0530

    Add clear button and error if dividing by 0

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 const equalsButton = document.getElementById("equals");
 const allClearButton = document.getElementById("all-clear");
+const clearButton = document.getElementById("clear");
 const deleteButton = document.getElementById("delete");
 
 // Add event listeners to the number buttons, operator buttons, and special buttons (equals, C, AC, DEL)
@@ -26,9 +27,8 @@ operatorButtons.forEach(button => {
 });
 
 equalsButton.addEventListener("click", () => isEqualTo());
-
 allClearButton.addEventListener("click", () => clearAll());
-
+clearButton.addEventListener("click", () => resetDisplay());
 deleteButton.addEventListener("click", () => deleteLastDigit());
 
 // Mathematical functions that are called on by operate()
@@ -70,6 +70,7 @@ function appendValue(value) {
 // Stores the clicked operator and operand 1, and executes the operation if an operator is already present
 function setOperator(operator) {
     if (currentOperator !== null) runOperation();
+
     currentOperator = operator;
     operand1 = calcDisplay.textContent;
     shouldResetDisplay = true; // So that the display resets when entering a new number
@@ -79,11 +80,23 @@ function setOperator(operator) {
 // Executes the operation and resets the current operator
 function runOperation() {
     if (shouldResetDisplay) return;
+
+    if (calcDisplay.textContent === "0" && currentOperator === "/") {
+        alert("Don't try to divide by zero! It makes the computer unhappy :(");
+        return;
+    }
+
     operand2 = calcDisplay.textContent;
     // Update display with correct result
-    calcDisplay.textContent = operate(Number(operand1), Number(operand2), currentOperator);
+    calcDisplay.textContent = roundNumber(operate(Number(operand1), Number(operand2), currentOperator), 3); // Rounds to 3 decimal places
     currentOperator = null;
     return;
+}
+
+// Rounds the input number to the given number of decimal places
+function roundNumber(num, decimalPlaces) {
+    let num2 = Math.pow(10, decimalPlaces);
+    return Math.round((num * num2)) / num2;
 }
 
 // Resets the calculator display when called
@@ -112,8 +125,8 @@ function clearAll() {
 
 // Deletes the last digit of the entered number
 function deleteLastDigit() {
-    if (shouldResetDisplay === true) resetDisplay(); // Resets display if a 
-    calcDisplay.textContent = calcDisplay.textContent.slice(0, -1);
+    if (shouldResetDisplay === true) resetDisplay(); // Resets the display if an operation was just entered
+    calcDisplay.textContent = calcDisplay.textContent.slice(0, -1); // Deletes the last character of the display string
     if (calcDisplay.textContent === "") calcDisplay.textContent = "0";
     return;
 }
