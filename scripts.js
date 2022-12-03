e797ba93ef319831cbd43207b0b11ab9fd19fd63
Manoranjan Kumar <manoranjan.kumar@outlook.com>
2022-12-03 14:42:49 +0530

    Refactor JS and fix the equals button

--- scripts.js
+++ scripts.js
@@ -1,23 +1,22 @@
-let currentValue = "";
-let result = 0;
+//let currentValue = "";
+let currentOperator = null;
 let operand1 = null;
 let operand2 = null;
-let operator1 = "";
-let operator2 = "";
+let shouldResetDisplay = false;
+
+const calcDisplay = document.querySelector(".calc-display");
+const equalsButton = document.getElementById("equals-button");
 
 // Basic arithmetic functions
 function add(num1, num2) {
     return num1 + num2;
 }
-
 function subtract(num1, num2) {
     return num1 - num2;
 }
-
 function multiply(num1, num2) {
     return num1 * num2;
 }
-
 function divide(num1, num2) {
     return num1 / num2;
 }
@@ -36,64 +35,67 @@ function operate(num1, num2, operator) {
     }
 }
 
-// Sends the current value to the calculator display
-function displayValue(value) {
-    document.querySelector(".calc-display").textContent = value;
-    console.log(`currentValue: ${value}.`)
-}
-
-// Function
-function storeOperators(op) {
-    if (operator1 === "") {
-        operator1 = op;
-        console.log(`Stored operator1: ${operator1}.`)
-    }
-    else {
-        operator2 = op;
-        console.log(`Stored operator2: ${operator2}.`)
-    }
-    return;
-}
-
-// Function
-function storeOperands() {
-    if (operand1 === null) {
-        operand1 = Number(currentValue);
-        console.log(`Stored operand1: ${operand1}.`)
-        currentValue = "";
-        return;
-    }
-    if (operand2 === null) { 
-        operand2 = Number(currentValue);
-        console.log(`Stored operand2: ${operand2}.`)
-
-        operand1 = operate(operand1, operand2, operator1);
-        result = operand1;
-        console.log(`Result: ${result}.`)
-
-        operand2 = null;
-        operator1 = operator2;
-        operator2 = "";
-        currentValue = "";
-        displayValue(result);
-        return;
-    }
-}
-
-// Registers number input, corrects the current value, and calls displayValue()
-const operandButtons = document.querySelectorAll(".operand");
-operandButtons.forEach(button => {
+// Add event listeners to the number buttons that runs appendValue()
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-        currentValue += button.value;
-        displayValue(currentValue);
+        appendValue(button.value);        
     });
 });
 
-// When clicked, the operator buttons will
+// Add event listeners to the operator buttons
 const operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach(button => {
     button.addEventListener("click", () => {
-        storeOperators(button.value);
-        storeOperands();
+        setOperator(button.value);
     });
 });
+
+// Add event listener to the equals button
+equalsButton.addEventListener("click", () => {
+    isEqualTo();
+})
+
+// Appends the clicked number to the current value and displays it
+function appendValue(value) {
+    if (shouldResetDisplay === true) resetDisplay(); // Resets display if an operation was performed
+    if (calcDisplay.textContent === "0") calcDisplay.textContent = ""; // Removes the starting '0'
+    calcDisplay.textContent += value;
+    return;
+}
+
+// Stores the clicked operator
+function setOperator(operator) {
+    if (currentOperator !== null) runOperation();
+    currentOperator = operator;
+    operand1 = calcDisplay.textContent;
+    shouldResetDisplay = true;
+    return;
+}
+
+// Executes the operation, resets the operator
+function runOperation() {
+    // If there's already an operator, don't do anything
+    if (shouldResetDisplay) return;
+    // Grab current number as operand 2
+    operand2 = calcDisplay.textContent;
+    // Update display with solved number, then reset current operator
+    calcDisplay.textContent = operate(Number(operand1), Number(operand2), currentOperator);
+    currentOperator = null;
+    return;
+}
+
+// Resets the calculator display
+function resetDisplay() {
+    calcDisplay.textContent = "0";
+    shouldResetDisplay = false;
+    return;
+}
+
+// Enables equal button functionality
+function isEqualTo() {
+    if (currentOperator === null) return;
+    runOperation();
+    shouldResetDisplay = true;
+    return;
+}
